2e58b71793bb0b85f0e3e55d638e16cdc9be7980
lagupa <lagustephen@gmail.com>
2019-08-02 19:49:45 -0500

    Functional Component

---
+++ src/components/react_basics/reactBasics.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+//Note
+/* 
+- always use uppercase and camelcase for name 
+fucntion and class comps 
+- func comp has no render method, it only returns
+
+*/
+
+function MyFunc() {
+    return (
+        <div>
+            <h1>React Functional Component</h1>
+            <p>This is a functional component</p>
+        </div>
+    )
+}
+
+export default MyFunc
